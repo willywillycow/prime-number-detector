97f177ca044bbf1472d1ee26e84950aff43d6550
willywillycow <66863816+willywillycow@users.noreply.github.com>
2021-02-06 00:17:20 +0100

    Prime number detector
    This is a basic function of how to detect prime numbers in python.

--- prime number detector.py
+++ prime number detector.py
@@ -1,9 +1,12 @@
 def isprime(number):
     isprime = None
-    for chosed in range(2, number):
-        if number % chosed == 0:
-            isprime = False
-            break
-        elif chosed == number - 1:
-            isprime = True
+    if number != 2:
+        for chosed in range(2, number):
+            if number % chosed == 0:
+                isprime = False
+                break
+            elif chosed == number - 1:
+                isprime = True
+    elif number == 2:
+        isprime = True
     return isprime
